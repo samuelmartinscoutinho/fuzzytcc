b690ac311d3a164e677097b7d45ad3b6fbfc021f
Samuel <samuelcoutinhodev@gmail.com>
2023-02-15 11:32:11 -0300

    Arrumado nome arquivo config e adicionado extensao do banco de dados.

--- fuzzytcc/app.py
+++ fuzzytcc/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from fuzzytcc.ext import config
+from fuzzytcc.ext import configuracao
 
 def minimal_app(**config):
     app = Flask(__name__)
-    config.init_app(app, **config)
+    configuracao.init_app(app, **config)
     return app
 
 def create_app(**config):
     app = minimal_app(**config)
-    config.carregar_extensoes(app)
+    configuracao.carregar_extensoes(app)
     return 
