056699dbd3699fa630a27997dca97f06c62042ea
Samuel <samuelcoutinhodev@gmail.com>
2023-02-16 23:40:39 -0300

    Alterado o menu lateral.

--- fuzzytcc/static/js/sidebar.js
+++ fuzzytcc/static/js/sidebar.js
@@ -1,37 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
-    const showNavbar = (toggleId, navId, bodyId, headerId) => {
-        const toggle = document.getElementById(toggleId),
-            nav = document.getElementById(navId),
-            bodypd = document.getElementById(bodyId),
-            headerpd = document.getElementById(headerId)
-
-        // Validate that all variables exist
-        if (toggle && nav && bodypd && headerpd) {
-            toggle.addEventListener('click', () => {
-                // show navbar
-                nav.classList.toggle('show')
-                    // change icon
-                toggle.classList.toggle('bx-x')
-                    // add padding to body
-                bodypd.classList.toggle('body-pd')
-                    // add padding to header
-                headerpd.classList.toggle('body-pd')
-            })
-        }
+    let arrow = document.querySelectorAll(".arrow");
+    for (var i = 0; i < arrow.length; i++) {
+        arrow[i].addEventListener("click", (e) => {
+            let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
+            arrowParent.classList.toggle("showMenu");
+        });
     }
-
-    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
-
-    /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.nav_link')
-
-    function colorLink() {
-        if (linkColor) {
-            linkColor.forEach(l => l.classList.remove('active'))
-            this.classList.add('active')
-        }
-    }
-    linkColor.forEach(l => l.addEventListener('click', colorLink))
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    sidebarBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    });
 
 });
